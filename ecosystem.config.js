04015ff8e6b9c88f053b8406c0eec7fa35333861
Rahil-Huseynov <huseynov.rahil.05@gmail.com>
2026-01-30 17:49:02 +0000

    feat: ecosystem for server and docker backup logic

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,16 @@
 module.exports = {
   apps: [
     {
-      name: 'nest-server',
-      script: 'npm',
-      args: 'run start:prod',
-      instances: 'max',
-      exec_mode: 'cluster',
+      name: "exam-backend",
+      script: "dist/src/main.js",
+      instances: 1,
+      autorestart: true,
       watch: false,
-      max_memory_restart: '1G',
-    },
-  ],
+      cwd: "/mnt/Disk_1TB/Exam_Website/Exam_Website_Backend",
+      env: {
+        NODE_ENV: "production",
+        PYTHON_CMD: "/mnt/Disk_1TB/Exam_Website/Exam_Website_Backend/.venv/bin/python"
+      }
+    }
+  ]
 };
